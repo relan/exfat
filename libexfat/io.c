ac56bfb2e17e752e1fec0f918723c69a91df3a25
relan <relan@users.noreply.github.com>
2010-01-08 09:01:24 +0000

    Update atime on read.

--- libexfat/io.c
+++ libexfat/io.c
@@ -67,6 +67,8 @@ ssize_t exfat_read(const struct exfat* ef, struct exfat_node* node,
 		remainder -= lsize;
 		cluster = exfat_next_cluster(ef, node, cluster);
 	}
+	if (!ef->ro)
+		exfat_update_atime(node);
 	return size - remainder;
 }
 
