291c91d56342e400a9636e6d26df6ba353a407a9
relan <relan@users.noreply.github.com>
2010-01-10 19:53:49 +0000

    Changed source files headings to meet the FSF recommendations.

--- SConstruct
+++ SConstruct
@@ -1,10 +1,21 @@
 #
-#  SConstruct
-#  SConscript for all components.
+#	SConstruct (10.09.09)
+#	SConscript for all components.
 #
-#  Created by Andrew Nayenko on 10.09.09.
-#  This software is distributed under the GNU General Public License 
-#  version 3 or any later.
+#	Copyright (C) 2009, 2010  Andrew Nayenko
+#
+#	This program is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 import os
--- libexfat/node.c
+++ libexfat/node.c
@@ -1,11 +1,22 @@
 /*
- *  node.c
- *  exFAT file system implementation library.
- *
- *  Created by Andrew Nayenko on 09.10.09.
- *  This software is distributed under the GNU General Public License 
- *  version 3 or any later.
- */
+	node.c (09.10.09)
+	exFAT file system implementation library.
+
+	Copyright (C) 2009, 2010  Andrew Nayenko
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "exfat.h"
 #include <errno.h>
