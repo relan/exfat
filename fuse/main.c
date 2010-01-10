a43ba0c36d311dcb8e1f953e21760b8d1a902429
relan <relan@users.noreply.github.com>
2010-01-10 19:53:49 +0000

    Change source files headings to meet FSF recommendations.

--- fuse/main.c
+++ fuse/main.c
@@ -1,11 +1,22 @@
 /*
- *  main.c
- *  FUSE-based exFAT implementation. Requires FUSE 2.6 or later.
- *
- *  Created by Andrew Nayenko on 01.09.09.
- *  This software is distributed under the GNU General Public License 
- *  version 3 or any later.
- */
+	main.c (01.09.09)
+	FUSE-based exFAT implementation. Requires FUSE 2.6 or later.
+
+	Copyright (C) 2009, 2010  Andrew Nayenko
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include <fuse.h>
 #include <errno.h>
--- libexfat/exfat.h
+++ libexfat/exfat.h
@@ -1,12 +1,23 @@
 /*
- *  exfat.h
- *  Definitions of structures and constants used in exFAT file system
- *  implementation.
- *
- *  Created by Andrew Nayenko on 29.08.09.
- *  This software is distributed under the GNU General Public License 
- *  version 3 or any later.
- */
+	exfat.h (29.08.09)
+	Definitions of structures and constants used in exFAT file system
+	implementation.
+
+	Copyright (C) 2009, 2010  Andrew Nayenko
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef EXFAT_H_INCLUDED
 #define EXFAT_H_INCLUDED
