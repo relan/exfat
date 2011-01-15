b36653b70459c30dc1d9c65669a94d98b862d776
relan <relan@users.noreply.github.com>
2011-01-15 14:19:31 +0000

    Removed extra includes in sbdump.

--- sbdump/main.c
+++ sbdump/main.c
@@ -19,8 +19,6 @@
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <inttypes.h>
