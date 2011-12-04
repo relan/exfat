bd3c9be7130e1fcb205b267a15141d8e3c8f0ad9
relan <relan@users.noreply.github.com>
2011-12-04 10:32:04 +0000

    Update allocated space percent on unmount.

--- libexfat/mount.c
+++ libexfat/mount.c
@@ -279,6 +279,18 @@ static void finalize_super_block(struct exfat* ef)
 
 	ef->sb->volume_state = cpu_to_le16(
 			le16_to_cpu(ef->sb->volume_state) & ~EXFAT_STATE_MOUNTED);
+
+	/* Some implementations set the percentage of allocated space to 0xff
+	   on FS creation and never update it. In this case leave it as is. */
+	if (ef->sb->allocated_percent != 0xff)
+	{
+		uint32_t free, total;
+
+		free = exfat_count_free_clusters(ef);
+		total = le32_to_cpu(ef->sb->cluster_count);
+		ef->sb->allocated_percent = ((total - free) * 100 + total / 2) / total;
+	}
+
 	commit_super_block(ef);
 }
 
