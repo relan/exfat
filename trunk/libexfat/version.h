8d1eabe1c92f8f625785e132d80d116f6554c552
resver <resver@60bc1c72-a15a-11de-b98f-4500b42dc123>
2013-01-19 13:11:28 +0000

    Updated copyright years.

--- trunk/libexfat/version.h
+++ trunk/libexfat/version.h
@@ -2,7 +2,7 @@
 	version.h (12.06.10)
 	Version constants.
 
-	Copyright (C) 2010-2012  Andrew Nayenko
+	Copyright (C) 2010-2013  Andrew Nayenko
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
