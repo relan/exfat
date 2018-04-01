0fb715da0469733e71b362f0e5f979ddf4e60e2d
Tomohiro Kusumi <kusumi.tomohiro@gmail.com>
2018-04-01 14:50:58 +0900

    Fix predefined macro for DragonFlyBSD
    __DragonFlyBSD__ is not defined in DragonFlyBSD.
    It's __DragonFly__ without "BSD".

--- libexfat/platform.h
+++ libexfat/platform.h
@@ -46,7 +46,7 @@
 #define EXFAT_LITTLE_ENDIAN LITTLE_ENDIAN
 #define EXFAT_BIG_ENDIAN BIG_ENDIAN
 
-#elif defined(__FreeBSD__) || defined(__DragonFlyBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
+#elif defined(__FreeBSD__) || defined(__DragonFly__) || defined(__NetBSD__) || defined(__OpenBSD__)
 
 #include <sys/endian.h>
 #define exfat_bswap16(x) bswap16(x)
