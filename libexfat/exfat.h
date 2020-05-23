02b793c0bd40f9474f3d6f6f4c52a4f4fc631f3c
relan <relan@users.noreply.github.com>
2020-05-23 18:34:51 +0300

    Move match_option() to libexfat/utils.c.
    To be used in a later patch.

--- libexfat/exfat.h
+++ libexfat/exfat.h
@@ -201,6 +201,7 @@ le16_t exfat_calc_name_hash(const struct exfat* ef, const le16_t* name,
 void exfat_humanize_bytes(uint64_t value, struct exfat_human_bytes* hb);
 void exfat_print_info(const struct exfat_super_block* sb,
 		uint32_t free_clusters);
+bool exfat_match_option(const char* options, const char* option_name);
 
 int exfat_utf16_to_utf8(char* output, const le16_t* input, size_t outsize,
 		size_t insize);
